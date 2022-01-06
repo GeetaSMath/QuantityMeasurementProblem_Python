d43670dfa04ce368552443bb415efc1447a16f8b
GeetaSMath <geetasmath@gmail.com>
2022-01-06 13:34:43 +0530

    Added:ReferenceCheck for the length

--- quantity_measurement_problem.py
+++ quantity_measurement_problem.py
@@ -2,11 +2,22 @@ from quantity_measurement_exception import QuantityMeasurementException
 
 class QuantityMeasurementProblem:
     def __init__(self, length):
+        """
+         init method to pass length arguments
+        :param length: length
+        """
         self.length = length
 
     def __eq__(self, other):
+        """
+         method find quality length for none
+        :param other: other
+        :return: others and inches and raise the exception
+        """
         if other.length is None:
-            raise QuantityMeasurementException('Null')
+            raise QuantityMeasurementException('none')
+        elif other.length != self.length:
+            raise QuantityMeasurementException('References are not Equal')
         elif other.length > 0:
             return other.length * 12
         else:
--- tests/test_quantity_measurement.py
+++ tests/test_quantity_measurement.py
@@ -10,8 +10,24 @@ class TestQuantityMeasurementProblem:
 
     @staticmethod
     def test_null_check():
+        """
+        test case to find null check and comparing length1 and length
+        :return: none
+        """
         with pytest.raises(QuantityMeasurementException) as exe:
             length1 = QuantityMeasurementProblem(0)
             length2 = QuantityMeasurementProblem(None)
             length1 == length2
-        assert exe.value.message == 'Null'
+        assert exe.value.message == 'none'
+
+    @staticmethod
+    def test_reference_check():
+        """
+        test reference check none
+        :return: reference not qual
+        """
+        with pytest.raises(QuantityMeasurementException) as exe:
+            ref1 = QuantityMeasurementProblem(1)
+            ref2 = QuantityMeasurementProblem(2)
+            ref1 != ref2
+        assert exe.value.message == 'References are not Equal'
