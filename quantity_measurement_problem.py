75f924a77c7967070f3422caac4040915af15572
GeetaSMath <geetasmath@gmail.com>
2022-01-07 20:55:42 +0530

    Added:to check yard and feet check

--- quantity_measurement_problem.py
+++ quantity_measurement_problem.py
@@ -1,26 +1,44 @@
 from quantity_measurement_exception import QuantityMeasurementException
 
-class QuantityMeasurementProblem:
-    def __init__(self, length):
+
+class QuantityMeasurement:
+    def __init__(self, length, unit):
         """
-         init method to pass length arguments
-        :param length: length
+
+        :param length: passing length
+        :param unit: passing unit
         """
         self.length = length
+        self.unit = unit
+
+    @staticmethod
+    def feat_fun():
+        FEET = 3
+        return FEET
+
+    @staticmethod
+    def yard_fun():
+        YARD = 1
+        return YARD
 
     def __eq__(self, other):
         """
-         method find quality length for none
-        :param other: other
-        :return: others and inches and raise the exception
+
+        :param other: passing param
+        :return: checking for quantity length unit
         """
+        yard = QuantityMeasurement.yard_fun()
+        feat = QuantityMeasurement.feat_fun()
         if other.length is None:
-            raise QuantityMeasurementException('none')
-        if other.length != self.length:
-            raise QuantityMeasurementException('References are not Equal')
-        if type(other.length) != (self.length):
+            raise QuantityMeasurementException('Null')
+        elif self.unit == other.unit or other.length == self.length:
+            print("Values Not Equal")
+            return True
+        elif type(self.length) != type(other.length):
             raise QuantityMeasurementException('Type Not Equal')
-        if other.length > 0:
-            return other.length * 12
+        elif self.length != other.length:
+            raise QuantityMeasurementException('References are not Equal')
+        elif other.length > 0:
+            return other.length / 3
         else:
-            raise QuantityMeasurementException('Length is Invalid')
+            raise QuantityMeasurementException("Invalid")
--- tests/test_quantity_measurement.py
+++ tests/test_quantity_measurement.py
@@ -1,45 +1,90 @@
 import pytest
-from quantity_measurement_problem import QuantityMeasurementProblem
+from quantity_measurement_problem import QuantityMeasurement
 from quantity_measurement_exception import QuantityMeasurementException
 
 
 class TestQuantityMeasurementProblem:
-    @pytest.fixture()
-    def test_quantity_measurement(self):
-        self.length = QuantityMeasurementProblem()
+        """
+         test cases to check values for zero and type and reference check
+         by its lenth and unit
+        """
 
-    @staticmethod
-    def test_null_check():
-        """
-        test case to find null check and comparing length1 and length
-        :return: none
-        """
-        with pytest.raises(QuantityMeasurementException) as exe:
-            length1 = QuantityMeasurementProblem(0)
-            length2 = QuantityMeasurementProblem(None)
-            length1 == length2
-        assert exe.value.message == 'none'
+        @pytest.mark.parametrize("length1,unit1,length2,unit2",[(0,"feet",0,"feet")])
+        def test_zero_value_check(self, length1, unit1, length2, unit2):
+            """
 
-    @staticmethod
-    def test_reference_check():
-        """
-        test reference check none
-        :return: reference not qual
-        """
-        with pytest.raises(QuantityMeasurementException) as exe:
-            ref1 = QuantityMeasurementProblem(1)
-            ref2 = QuantityMeasurementProblem(2)
-            ref1 != ref2
-        assert exe.value.message == 'References are not Equal'
+            :param length1: length contains 1 and equals to 12 inches
+            :param unit1: unit is to check for reference
+            :param length2: param for check
+            :param unit2:  reference check
+            :return: pass quantity when we compare
+            """
+            print(length1)
+            quantity1 = QuantityMeasurement(length1, unit1)
+            quantity2 = QuantityMeasurement(length2, unit2)
+            assert quantity1 == quantity2
 
-    @staticmethod
-    def test_type_check():
-        """
-         type check for type1 and type2
-        :return: not equal
-        """
-        with pytest.raises(QuantityMeasurementException) as exe:
-            type1 = QuantityMeasurementProblem(1)
-            type2 = QuantityMeasurementProblem(1)
-            type1 != type2
-        assert exe.value.message == 'Type Not Equal'
+        @pytest.mark.parametrize('length1, unit1, length2, unit2', [(None, "feet", None, "feet")])
+        def test_null_reference_check(self, length1, unit1, length2, unit2):
+            """
+
+            :param length1: length1 for none
+            :param unit1: unit to find feet
+            :param length2: length2 for none
+            :param unit2:unit to find feet
+            :return: pass the test case when it true
+            """
+            quantity1 = QuantityMeasurement(length1, unit1)
+            quantity2 = QuantityMeasurement(length2, unit2)
+            with pytest.raises(QuantityMeasurementException) as exe:
+                quantity1 == quantity2
+            assert exe.value.message == 'Null'
+
+        @pytest.mark.parametrize('length1, unit1, length2, unit2', [(0, "feet", 2, "inch")])
+        def test_reference_check(self, length1, unit1, length2, unit2):
+            """
+            test case for reference check
+            :param length1:length1 is for 0
+            :param unit1: unit is for feet
+            :param length2: length2 is for 2
+            :param unit2: inch unit
+            :return: pass the test cases
+            """
+            quantity1 = QuantityMeasurement(length1, unit1)
+            quantity2 = QuantityMeasurement(length2, unit2)
+            with pytest.raises(QuantityMeasurementException) as exe:
+                quantity1 == quantity2
+            assert exe.value.message == 'References are not Equal'
+
+        @pytest.mark.parametrize('length1, unit1 , length2, unit2', [(0, "feet", "hello", "inch")])
+        def test_type_check(self, length1, unit1, length2, unit2):
+            """
+            test case for type check
+            :param length1: length1 is for zero
+            :param unit1: unit for feet
+            :param length2: length2 is for str to pass
+            :param unit2: inch for unit
+            :return: pass the test case when its true
+            """
+            quantity1 = QuantityMeasurement(length1, unit1)
+            quantity2 = QuantityMeasurement(length2, unit2)
+            with pytest.raises(QuantityMeasurementException) as exe:
+                quantity1 == quantity2
+            assert exe.value.message == 'Type Not Equal'
+
+        @pytest.mark.parametrize('length1, unit1 , length2, unit2', [(0, "feet", "hello", "inch")])
+        def test_type_check(self, length1, unit1, length2, unit2):
+            """
+             test case fot type check is equal
+            :param length1: for zero
+            :param unit1: for feet
+            :param length2: to pass string
+            :param unit2: inch to find unit
+            :return: pass the test case when its true
+            """
+            quantity1 = QuantityMeasurement(length1, unit1)
+            quantity2 = QuantityMeasurement(length2, unit2)
+            with pytest.raises(QuantityMeasurementException) as exe:
+                quantity1 == quantity2
+            assert exe.value.message == 'Type Not Equal'
+
